be5296742beed256656dde9f33a34ec3c6e24922
hugo de araujo <hugo.saraujo@tutanota.com>
2024-07-07 21:18:02 -0300

    Criação do método de requisição POST no FilmController.

---
+++ Filmes.Api/Controllers/FilmController.cs
@@ -0,0 +1,18 @@
+﻿using Filmes.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmController : ControllerBase
+{
+    private static List<Film> _films = new List<Film>();
+
+    [HttpPost]
+    public void AddFilm(Film film)
+    {
+        _films.Add(film);
+        Console.WriteLine("Filme Adicionado!");
+    }
+}
